6b74013c2c1063c025665c6763614c34af46b3a2
Chatpanai Keadsomjit <Chatpanai.kea@dome.tu.ac.th>
2025-02-21 21:28:20 +0700

    add table

--- backend/database/db.ts
+++ backend/database/db.ts
@@ -4,6 +4,12 @@ import {
   CREATE_SESSION_TABLE,
   CREATE_USER_TABLE,
   CREATE_VERIFICATION_TABLE,
+  CREATE_ADDRESS_TABLE,
+  CREATE_PAYMENT_TABLE,
+  CREATE_RENTAL_TABLE,
+  CREATE_ITEM_TABLE,
+  CREATE_CATEGORY_TABLE,
+  CREATE_REVIEW_TABLE,
 } from "./sql.ts";
 
 const sql = postgres({
@@ -20,4 +26,10 @@ await sql.begin(async (sql) => {
   await sql.unsafe(CREATE_ACCOUNT_TABLE);
   await sql.unsafe(CREATE_SESSION_TABLE);
   await sql.unsafe(CREATE_VERIFICATION_TABLE);
+  await sql.unsafe(CREATE_ADDRESS_TABLE);
+  await sql.unsafe(CREATE_PAYMENT_TABLE);
+  await sql.unsafe(CREATE_RENTAL_TABLE);
+  await sql.unsafe(CREATE_ITEM_TABLE);
+  await sql.unsafe(CREATE_CATEGORY_TABLE);
+  await sql.unsafe(CREATE_REVIEW_TABLE);
 });
--- backend/database/sql.ts
+++ backend/database/sql.ts
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS
     name VARCHAR NOT NULL,
     email VARCHAR NOT NULL,
     email_verified BOOLEAN NULL DEFAULT false,
-    image VARCHAR NULL,
+    phone_number VARCHAR UNIQUE NULL,
     created_at TIMESTAMPTZ NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMPTZ NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT user_pkey PRIMARY KEY (id),
@@ -65,4 +65,94 @@ CREATE TABLE IF NOT EXISTS
     CONSTRAINT account_user_fk FOREIGN KEY (user_id) REFERENCES "user"(id) ON DELETE CASCADE
   );
 CREATE INDEX account_user_idx ON "account"(user_id);
+`;
+
+
+export const CREATE_ADDRESS_TABLE = `
+CREATE TABLE IF NOT EXISTS
+  "address" (
+    address_id UUID DEFAULT gen_random_uuid(),
+    user_id VARCHAR NOT NULL,
+    postal_code VARCHAR NOT NULL,
+    residence_info TEXT NOT NULL,
+    district VARCHAR NOT NULL,
+    subdistrict VARCHAR NOT NULL,
+    province VARCHAR NOT NULL,
+    PRIMARY KEY (address_id),             
+    CONSTRAINT address_user_fk FOREIGN KEY (user_id) REFERENCES "user"(user_id) ON DELETE CASCADE
+  );
+`;
+
+export const CREATE_PAYMENT_TABLE = `
+CREATE TABLE IF NOT EXISTS
+  "payment" (
+    payment_id UUID DEFAULT gen_random_uuid(),
+    user_id VARCHAR NOT NULL,
+    status TEXT NOT NULL,
+    amount DECIMAL(10,2) NOT NULL,
+    rental_id VARCHAR UNIQUE,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (payment_id),
+    CONSTRAINT payment_user_fk FOREIGN KEY (user_id) REFERENCES "user"(user_id) ON DELETE CASCADE,
+    CONSTRAINT payment_rental_fk FOREIGN KEY (rental_id) REFERENCES "rental"(rental_id) ON DELETE CASCADE
+  );
+`;
+
+export const CREATE_RENTAL_TABLE = `
+CREATE TABLE IF NOT EXISTS
+  "rental" (
+    rental_id UUID DEFAULT gen_random_uuid(),
+    user_id VARCHAR NOT NULL,
+    status TEXT NOT NULL,
+    total_price DECIMAL(10,2) NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (rental_id),
+    CONSTRAINT rental_user_fk FOREIGN KEY (user_id) REFERENCES "user"(user_id) ON DELETE CASCADE
+  );
+`;
+
+export const CREATE_ITEM_TABLE = `
+CREATE TABLE IF NOT EXISTS
+  "item" (
+    item_id UUID DEFAULT gen_random_uuid(),
+    user_id VARCHAR NOT NULL,
+    item_name TEXT NOT NULL,
+    description TEXT,
+    status TEXT NOT NULL,
+    price_per_day DECIMAL(10,2) NOT NULL,
+    category_id VARCHAR NOT NULL,
+    rental_id VARCHAR NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (item_id),
+    CONSTRAINT item_user_fk FOREIGN KEY (user_id) REFERENCES "user"(user_id) ON DELETE CASCADE,
+    CONSTRAINT item_category_fk FOREIGN KEY (category_id) REFERENCES "category"(category_id) ON DELETE CASCADE,
+    CONSTRAINT item_rental_fk FOREIGN KEY (rental_id) REFERENCES "rental"(rental_id) ON DELETE CASCADE
+  );
+`;
+
+export const CREATE_CATEGORY_TABLE = `
+CREATE TABLE IF NOT EXISTS
+  "category" (
+    category_id UUID DEFAULT gen_random_uuid(),
+    category_name TEXT NOT NULL UNIQUE,
+    PRIMARY KEY (category_id)
+  );
+`;
+
+export const CREATE_REVIEW_TABLE = `
+CREATE TABLE IF NOT EXISTS
+  "review" (
+    review_id UUID DEFAULT gen_random_uuid()
+    item_id VARCHAR NOT NULL,
+    user_id VARCHAR NOT NULL,
+    rating INT CHECK (rating BETWEEN 1 AND 5),
+    comment TEXT,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (review_id, item_id),
+    CONSTRAINT review_item_fk FOREIGN KEY (item_id) REFERENCES "item"(item_id) ON DELETE CASCADE,
+    CONSTRAINT review_user_fk FOREIGN KEY (user_id) REFERENCES "user"(user_id) ON DELETE CASCADE,
+    CONSTRAINT unique_user_item_review UNIQUE (user_id, item_id)
+  );
 `;
