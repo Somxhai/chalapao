0c0d2eac7d07fc2fe8cd943b59b9cfd92c0ad803
= <ChanakanThongrong@gmail.com>
2025-02-28 14:43:00 +0700

    fix(db): reorder table and add separate sql files

--- backend/database/db.ts
+++ backend/database/db.ts
@@ -1,5 +1,23 @@
 import { Pool } from "deno-postgres";
-import * as SQL from "./sql.ts";
+import {
+  CREATE_CATEGORY_TABLE,
+  CREATE_ITEM_TABLE,
+  CREATE_PAYMENT_TABLE,
+  CREATE_RENTAL_TABLE,
+  CREATE_REVIEW_TABLE,
+} from "./sql/app.ts";
+
+import {
+  CREATE_ADDRESS_TABLE,
+  CREATE_USER_INFO_TABLE,
+} from "./sql/user_info.ts";
+
+import {
+  CREATE_ACCOUNT_TABLE,
+  CREATE_SESSION_TABLE,
+  CREATE_USER_TABLE,
+  CREATE_VERIFICATION_TABLE,
+} from "./sql/user.ts";
 
 const pool = new Pool(
   {
@@ -16,14 +34,28 @@ const pool = new Pool(
 export const client = await pool.connect();
 
 {
-  await client.queryArray(SQL.CREATE_USER_TABLE);
-  await client.queryArray(SQL.CREATE_ACCOUNT_TABLE);
-  await client.queryArray(SQL.CREATE_SESSION_TABLE);
-  await client.queryArray(SQL.CREATE_VERIFICATION_TABLE);
-  await client.queryArray(SQL.CREATE_ADDRESS_TABLE);
-  await client.queryArray(SQL.CREATE_ITEM_TABLE);
-  await client.queryArray(SQL.CREATE_RENTAL_TABLE);
-  await client.queryArray(SQL.CREATE_PAYMENT_TABLE);
-  await client.queryArray(SQL.CREATE_REVIEW_TABLE);
-  await client.queryArray(SQL.CREATE_CATEGORY_TABLE);
+  // 1. User tables first
+  await client.queryArray(CREATE_USER_TABLE);
+  await client.queryArray(CREATE_VERIFICATION_TABLE);
+  await client.queryArray(CREATE_SESSION_TABLE);
+  await client.queryArray(CREATE_ACCOUNT_TABLE);
+
+  // 2. User info and address
+  await client.queryArray(CREATE_USER_INFO_TABLE);
+  await client.queryArray(CREATE_ADDRESS_TABLE);
+
+  // 3. Category (Independent)
+  await client.queryArray(CREATE_CATEGORY_TABLE);
+
+  // 4. Items (Depends on category and user)
+  await client.queryArray(CREATE_ITEM_TABLE);
+
+  // 5. Rental (Depends on user)
+  await client.queryArray(CREATE_RENTAL_TABLE);
+
+  // 6. Payment (Depends on rental & user)
+  await client.queryArray(CREATE_PAYMENT_TABLE);
+
+  // 7. Review (Depends on item & user)
+  await client.queryArray(CREATE_REVIEW_TABLE);
 }
