2a287bce39d4d5f70927be280ab8d62f6cabfb77
= <ChanakanThongrong@gmail.com>
2025-03-22 23:51:52 +0700

    feat(sql): split review to user and item, remove total price in rental

--- backend/database/sql/app.ts
+++ backend/database/sql/app.ts
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS "rental" (
     delivery_address VARCHAR NOT NULL,
     payment_id VARCHAR NOT NULL,
     status TEXT NOT NULL,
-    total_price DECIMAL(10,2) NOT NULL,
+    total_price NUMERIC NOT NULL,
     start_date DATE NOT NULL,
     end_date DATE NOT NULL,
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
@@ -43,7 +43,7 @@ CREATE TABLE IF NOT EXISTS
     rental_terms TEXT NOT NULL,
     penalty_terms TEXT NOT NULL,
     item_status TEXT NOT NULL,
-    price_per_day DECIMAL(10,2) NOT NULL,
+    price_per_day NUMERIC NOT NULL,
     category_id UUID NOT NULL,
     item_rating FLOAT DEFAULT 0 CHECK (item_rating BETWEEN 0 AND 5),
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
@@ -64,26 +64,35 @@ CREATE TABLE IF NOT EXISTS
   );
 `;
 
-export const CREATE_REVIEW_TABLE = `
+export const CREATE_REVIEW_USER_TABLE = `
 CREATE TABLE IF NOT EXISTS
-  "review" (
+  "review_user" (
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     reviewer_id VARCHAR NOT NULL,
-    item_id UUID,
-    user_id VARCHAR,
+    user_id VARCHAR NOT NULL,
+    rating FLOAT CHECK (rating BETWEEN 0 AND 5),
+    comment TEXT,
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT target_user_review FOREIGN KEY (user_id) REFERENCES "user"(id) ON DELETE CASCADE,
+    CONSTRAINT review_by FOREIGN KEY (reviewer_id) REFERENCES "user"(id) ON DELETE CASCADE
+  );
+`;
+
+export const CREATE_REVIEW_ITEM_TABLE = `
+CREATE TABLE IF NOT EXISTS
+  "review_item" (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    reviewer_id VARCHAR NOT NULL,
+    item_id UUID NOT NULL,
     rating FLOAT CHECK (rating BETWEEN 0 AND 5),
     comment TEXT,
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMPTZ NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT target_item_review FOREIGN KEY (item_id) REFERENCES "item"(id) ON DELETE CASCADE,
-    CONSTRAINT target_user_review FOREIGN KEY (user_id) REFERENCES "user"(id) ON DELETE CASCADE,
-    CONSTRAINT review_by FOREIGN KEY (reviewer_id) REFERENCES "user"(id) ON DELETE CASCADE,
-
-    CHECK (
-      (user_id IS NOT NULL AND item_id IS NULL) OR
-      (user_id IS NULL AND item_id IS NOT NULL)
-    )
+    CONSTRAINT review_by FOREIGN KEY (reviewer_id) REFERENCES "user"(id) ON DELETE CASCADE
   );
 `;
 
@@ -100,7 +109,7 @@ export const CREATE_KEYWORD_TABLE = `
 CREATE TABLE IF NOT EXISTS "keyword"(
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     item_id UUID NOT NULL,
-    keyword TEXT NOT NULL PRIMARY KEY,
+    keyword TEXT NOT NULL,
     CONSTRAINT for FOREIGN KEY (item_id) REFERENCES "item"(id) ON DELETE CASCADE
   )
 `;
@@ -113,4 +122,3 @@ CREATE TABLE IF NOT EXISTS
     CONSTRAINT for FOREIGN KEY (item_id) REFERENCES "item"(id) ON DELETE CASCADE
   )
 `;
-
