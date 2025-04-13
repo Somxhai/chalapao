597cf6f48fe3afbd5e2288914b8fc82eaf4e766a
= <ChanakanThongrong@gmail.com>
2025-04-13 10:27:00 +0700

    feat(route): add item and category routes

--- backend/main.ts
+++ backend/main.ts
@@ -1,5 +1,35 @@
 import { Hono } from "hono";
 import "./database/db.ts";
+import { auth } from "./lib/auth.ts";
+import { HTTPException } from "hono/http-exception";
+import { itemApp } from "./handler/item.ts";
+import { categoryApp } from "./handler/category.ts";
+
 const app = new Hono();
+
+app.onError((err, c) => {
+  console.error(err);
+
+  if (err instanceof HTTPException) {
+    const res = err.getResponse();
+    return res;
+  }
+  return c.text(`Internal Server Error: ${err.message}`, 500);
+});
+
+app.on(["POST", "GET"], "/api/auth/*", (c) => {
+  return auth.handler(c.req.raw);
+});
+
+app.route("/item", itemApp);
+app.route("/category", categoryApp);
+
+// auth.api.signUpEmail({
+//   body: {
+//     name: "test-chalapao-" + crypto.randomUUID(),
+//     email: "test-chalapao-" + crypto.randomUUID() + "@testmail.test",
+//     password: "testpassword",
+//   },
+// });
 
 Deno.serve({ port: 8787 }, app.fetch);
