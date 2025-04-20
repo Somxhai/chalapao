34d4624f28b6729178ed4103f51fa3ee6aac76f3
= <ChanakanThongrong@gmail.com>
2025-04-20 15:28:35 +0700

    feat(db): use setupdb() instead

--- backend/database/db.ts
+++ backend/database/db.ts
@@ -2,36 +2,7 @@
 import pg from "npm:pg";
 
 const { Pool } = pg;
-
-import {
-  CREATE_CATEGORY_TABLE,
-  CREATE_ITEM_IMAGE_TABLE,
-  CREATE_ITEM_TABLE,
-  CREATE_KEYWORD_TABLE,
-  CREATE_PAYMENT_TABLE,
-  CREATE_RENTAL_ADDRESS_TABLE,
-  CREATE_RENTAL_TABLE,
-  CREATE_REVIEW_IMAGE_TABLE,
-  CREATE_REVIEW_ITEM_TABLE,
-  CREATE_REVIEW_USER_TABLE,
-} from "./sql/app.ts";
-
-import {
-  CREATE_ADDRESS_TABLE,
-  CREATE_USER_INFO_TABLE,
-} from "./sql/user_info.ts";
-
-import {
-  CREATE_ACCOUNT_TABLE,
-  CREATE_SESSION_TABLE,
-  CREATE_USER_TABLE,
-  CREATE_VERIFICATION_TABLE,
-} from "./sql/user.ts";
-
-import {
-  CREATE_UPDATE_AT_TRIGGER,
-  createUpdateAtTrigger,
-} from "./sql/trigger.ts";
+import { setupDatabase } from "../lib/db.ts";
 
 export const pool = new Pool({
   host: Deno.env.get("PGHOST") || "localhost",
@@ -43,47 +14,4 @@ export const pool = new Pool({
 
 const client = await pool.connect();
 
-try {
-  await client.query(CREATE_UPDATE_AT_TRIGGER);
-
-  await client.query(CREATE_USER_INFO_TABLE);
-  await client.query(CREATE_USER_TABLE);
-  await client.query(CREATE_VERIFICATION_TABLE);
-  await client.query(CREATE_SESSION_TABLE);
-  await client.query(CREATE_ACCOUNT_TABLE);
-
-  await client.query(CREATE_ADDRESS_TABLE);
-
-  await client.query(CREATE_CATEGORY_TABLE);
-  await client.query(CREATE_ITEM_TABLE);
-  await client.query(CREATE_PAYMENT_TABLE);
-  await client.query(CREATE_RENTAL_ADDRESS_TABLE);
-  await client.query(CREATE_RENTAL_TABLE);
-
-  await client.query(CREATE_REVIEW_USER_TABLE);
-  await client.query(CREATE_REVIEW_ITEM_TABLE);
-  await client.query(CREATE_REVIEW_IMAGE_TABLE);
-  await client.query(CREATE_KEYWORD_TABLE);
-  await client.query(CREATE_ITEM_IMAGE_TABLE);
-
-  const tables = [
-    "user",
-    "user_info",
-    "address",
-    "category",
-    "item",
-    "rental",
-    "payment",
-    "review",
-    "review_image",
-  ];
-
-  for (const table of tables) {
-    const triggerQuery = createUpdateAtTrigger(table);
-    await client.query(triggerQuery);
-  }
-} catch (error) {
-  console.error("Error creating tables:", error);
-} finally {
-  client.release();
-}
+await setupDatabase(client);
